53ca263c8611debe7c0c64ebb6e01f487f2389df
nedmorgan <nmorgan1987@gmail.com>
2019-04-12 14:31:26 -0400

    updated urls

--- message_clone/message_app/urls.py
+++ message_clone/message_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register('comments', views.CommentView)
+router.register('posts', views.PostView)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- message_clone/message_app/views.py
+++ message_clone/message_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
+
+
+class PostView(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentView(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
